f673cff6513ec952434e4126880eaeefd6a1c9ab
braeobro <brad.olson@movedbylight.com>
2018-05-20 19:03:46 -0400

    add(default asserter)

---
+++ init.go
@@ -0,0 +1,10 @@
+package au
+
+var (
+	// DefaultAsserter allows for simple assertions without creating an asserter.
+	DefaultAsserter = NewAsserter(false, 10, VerbosityDebug)
+	// Assert uses the DefaultAsserter to make assertions.
+	Assert = DefaultAsserter.Assert
+	// AssertSkip uses the DefaultAsserter to make assertions, optionally skipping frames in stack traces.
+	AssertSkip = DefaultAsserter.AssertSkip
+)
